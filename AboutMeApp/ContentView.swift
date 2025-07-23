ab6b80c8a6076a26f6fbf1201fdea168180bd244
Angelia Causing <aecausing@gmail.com>
2025-07-23 16:16:01 -0700

    debugging and added padding

--- AboutMeApp/ContentView.swift
+++ AboutMeApp/ContentView.swift
@@ -9,23 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color(.indigo)
-                .ignoresSafeArea()
-            VStack {
-                Text("Hi!! My name's Angelia Causing :D")
-                    .font(.title2)
-                    .fontWeight(.bold)
-                Image (.image)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                Text("I love watching motorsport events like Formula 1!")
-            }
-            
-            .padding()
-        }
+
+ZStack {
+    LinearGradient(colors: [.pink1, .orange1], startPoint: .top, endPoint: .bottom)
+        .ignoresSafeArea()
+   
+    VStack(spacing: 10.0) {
+        Text("Hi, I'm Angelia !")
+            .font(.title)
+            .fontWeight(.bold)
+        
+        Image ("angie")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+        
+        Text("I'm 17, and I'm currently in Los Angeles! I love baking, singing, dancing, and watching F1 races!")
+            .font(.body)
+            .fontWeight(.medium)
+            .multilineTextAlignment(.center)
     }
+    .padding(10.0)
+    .background(Color.white)
+    .cornerRadius(12)
+    .shadow(radius: 5)
+    . padding (.horizontal, 12.0)
 }
+        }
+                        }
 
 #Preview {
     ContentView()
